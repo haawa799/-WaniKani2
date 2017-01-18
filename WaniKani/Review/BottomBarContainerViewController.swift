269188c6468b745c5477f2789644b403f5c450d2
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-01-18 16:34:57 +0200

    Switch to using NJKScrollFullscreenKit

--- WaniKani/Review/BottomBarContainerViewController.swift
+++ WaniKani/Review/BottomBarContainerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import NJKScrollFullscreenKit
 
 protocol BottomBarContainerDelegate: class {
   func leftButtonPressed()
--- WaniKani/Utils/WaniKani-Bridging-Header.h
+++ WaniKani/Utils/WaniKani-Bridging-Header.h
@@ -2,8 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "NJKScrollFullScreen.h"
-#import "UIViewController+NJKFullScreenSupport.h"
 #import "RECommonFunctions.h"
 #import "RESideMenu.h"
 #import "UIViewController+RESideMenu.h"
