649444649cf12000284a9b579b73033d5713bc84
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-05-29 23:54:07 +0300

    Downloading

--- WaniKani/Data browser/DataBrowserCoordinator.swift
+++ WaniKani/Data browser/DataBrowserCoordinator.swift
@@ -12,120 +12,129 @@ import WaniModel
 
 class DataBrowserCoordinator: NSObject, Coordinator {
 
-    fileprivate let presenter: UINavigationController
-    fileprivate let searchDataProvider: SearchItemsDataProvider
-    fileprivate weak var dataBrowserViewController: DataBrowserViewController?
+  fileprivate let presenter: UINavigationController
+  fileprivate let searchDataProvider: SearchItemsDataProvider
+  fileprivate weak var dataBrowserViewController: DataBrowserViewController?
+  fileprivate var downloadingCoordinator: DownloadingCoordinator?
 
-    init(presenter: UINavigationController, persistance: Persistance) {
-        self.presenter = presenter
-        self.searchDataProvider = SearchItemsDataProvider(persistance: persistance)
-        super.init()
-        searchDataProvider.delegate = self
-        let tabItem: UITabBarItem = UITabBarItem(title: "Search", image: #imageLiteral(resourceName: "icon-search"), selectedImage: nil)
-        presenter.tabBarItem = tabItem
-    }
+  init(presenter: UINavigationController, persistance: Persistance) {
+    self.presenter = presenter
+    self.searchDataProvider = SearchItemsDataProvider(persistance: persistance)
+    super.init()
+    searchDataProvider.delegate = self
+    let tabItem: UITabBarItem = UITabBarItem(title: "Search", image: #imageLiteral(resourceName: "icon-search"), selectedImage: nil)
+    presenter.tabBarItem = tabItem
+  }
 
-    fileprivate func showKanji(kanji: KanjiInfo) {
-        let kanjiViewController: KanjiDetailViewController = KanjiDetailViewController.instantiateViewController()
-        kanjiViewController.kanji = kanji
-        kanjiViewController.navigationItem.title = kanji.character
-        presenter.pushViewController(kanjiViewController, animated: true)
-    }
+  fileprivate func showKanji(kanji: KanjiInfo) {
+    let kanjiViewController: KanjiDetailViewController = KanjiDetailViewController.instantiateViewController()
+    kanjiViewController.kanji = kanji
+    kanjiViewController.navigationItem.title = kanji.character
+    presenter.pushViewController(kanjiViewController, animated: true)
+  }
 
-    fileprivate func showRadical(radical: RadicalInfo) {
-        let radicalViewController: RadicalDetailViewController = RadicalDetailViewController.instantiateViewController()
-        radicalViewController.radical = radical
-        radicalViewController.navigationItem.title = radical.character
-        presenter.pushViewController(radicalViewController, animated: true)
-    }
+  fileprivate func showRadical(radical: RadicalInfo) {
+    let radicalViewController: RadicalDetailViewController = RadicalDetailViewController.instantiateViewController()
+    radicalViewController.radical = radical
+    radicalViewController.navigationItem.title = radical.character
+    presenter.pushViewController(radicalViewController, animated: true)
+  }
 
-    fileprivate func showWord(word: WordInfo) {
-        let wordDetailViewController: WordDetailViewController = WordDetailViewController.instantiateViewController()
-        wordDetailViewController.word = word
-        wordDetailViewController.navigationItem.title = word.character
-        presenter.pushViewController(wordDetailViewController, animated: true)
-    }
+  fileprivate func showWord(word: WordInfo) {
+    let wordDetailViewController: WordDetailViewController = WordDetailViewController.instantiateViewController()
+    wordDetailViewController.word = word
+    wordDetailViewController.navigationItem.title = word.character
+    presenter.pushViewController(wordDetailViewController, animated: true)
+  }
 }
 
 // MARK: - Coordinator
 extension DataBrowserCoordinator {
-    func start() {
-        let dataBrowserViewController: DataBrowserViewController = DataBrowserViewController.instantiateViewController()
-        _ = dataBrowserViewController.view
-        if presenter.traitCollection.forceTouchCapability == UIForceTouchCapability.available {
-            dataBrowserViewController.registerForPreviewing(with: self, sourceView: dataBrowserViewController.view)
-        }
-        dataBrowserViewController.delegate = self
-        presenter.isNavigationBarHidden = true
-        presenter.pushViewController(dataBrowserViewController, animated: false)
-        self.dataBrowserViewController = dataBrowserViewController
+  func start() {
+
+    let dataBrowserViewController: DataBrowserViewController = DataBrowserViewController.instantiateViewController()
+    _ = dataBrowserViewController.view
+    if presenter.traitCollection.forceTouchCapability == UIForceTouchCapability.available {
+      dataBrowserViewController.registerForPreviewing(with: self, sourceView: dataBrowserViewController.view)
     }
+    dataBrowserViewController.delegate = self
+    presenter.isNavigationBarHidden = true
+    presenter.pushViewController(dataBrowserViewController, animated: false)
+    self.dataBrowserViewController = dataBrowserViewController
+
+    let needsDownloading = true
+    if needsDownloading == true {
+      let downloadingCoordinator = DownloadingCoordinator(presenter: presenter)
+      downloadingCoordinator.start()
+      self.downloadingCoordinator = downloadingCoordinator
+    }
+  }
 }
 
 // MARK: - SearchItemsDataProviderDelegate
 extension DataBrowserCoordinator: SearchItemsDataProviderDelegate {
-    func newListViewModel(listViewModel: ListViewModel) {
-        dataBrowserViewController?.dataSource = listViewModel
-    }
+  func newListViewModel(listViewModel: ListViewModel) {
+    dataBrowserViewController?.dataSource = listViewModel
+  }
 }
 
 // MARK: - UIViewControllerPreviewingDelegate
 extension DataBrowserCoordinator: UIViewControllerPreviewingDelegate {
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
-        guard let dataBrowserViewController = dataBrowserViewController else { return nil }
-        let location = dataBrowserViewController.view.convert(location, to: dataBrowserViewController.collectionView)
-        guard let indexPath = dataBrowserViewController.collectionView?.indexPathForItem(at: location) else { return nil }
-        guard let cell = dataBrowserViewController.collectionView?.cellForItem(at: indexPath) else { return nil }
-        guard let item = dataBrowserViewController.dataSource?.cellDataItemForIndexPath(indexPath: indexPath)?.viewModel as? SearchItemCellViewModel else { return nil }
+  func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+    guard let dataBrowserViewController = dataBrowserViewController else { return nil }
+    let location = dataBrowserViewController.view.convert(location, to: dataBrowserViewController.collectionView)
+    guard let indexPath = dataBrowserViewController.collectionView?.indexPathForItem(at: location) else { return nil }
+    guard let cell = dataBrowserViewController.collectionView?.cellForItem(at: indexPath) else { return nil }
+    guard let item = dataBrowserViewController.dataSource?.cellDataItemForIndexPath(indexPath: indexPath)?.viewModel as? SearchItemCellViewModel else { return nil }
 
-        var viewController: UIViewController?
+    var viewController: UIViewController?
 
-        switch item.reviewItem {
-        case .kanji(let kanji):
-            let kanjiViewController: KanjiDetailViewController = KanjiDetailViewController.instantiateViewController()
-            kanjiViewController.kanji = kanji
-            let previewHeight = kanjiViewController.view.bounds.width + 15
-            kanjiViewController.preferredContentSize = CGSize(width: 0.0, height: previewHeight)
-            viewController = kanjiViewController
-        case .radical(let radical):
-            let radicalViewController: RadicalDetailViewController = RadicalDetailViewController.instantiateViewController()
-            radicalViewController.radical = radical
-            radicalViewController.navigationItem.title = radical.character
-            let previewHeight = radicalViewController.view.bounds.width * 0.5 + 15
-            radicalViewController.preferredContentSize = CGSize(width: 0.0, height: previewHeight)
-            viewController = radicalViewController
-        case .word(let word):
-            let wordDetailViewController: WordDetailViewController = WordDetailViewController.instantiateViewController()
-            wordDetailViewController.word = word
-            wordDetailViewController.navigationItem.title = word.character
-            let previewHeight = wordDetailViewController.view.bounds.width * 0.75 + 15
-            wordDetailViewController.preferredContentSize = CGSize(width: 0.0, height: previewHeight)
-            previewingContext.sourceRect = cell.frame
-            viewController = wordDetailViewController
-        }
-        return viewController
+    switch item.reviewItem {
+    case .kanji(let kanji):
+      let kanjiViewController: KanjiDetailViewController = KanjiDetailViewController.instantiateViewController()
+      kanjiViewController.kanji = kanji
+      let previewHeight = kanjiViewController.view.bounds.width + 15
+      kanjiViewController.preferredContentSize = CGSize(width: 0.0, height: previewHeight)
+      viewController = kanjiViewController
+    case .radical(let radical):
+      let radicalViewController: RadicalDetailViewController = RadicalDetailViewController.instantiateViewController()
+      radicalViewController.radical = radical
+      radicalViewController.navigationItem.title = radical.character
+      let previewHeight = radicalViewController.view.bounds.width * 0.5 + 15
+      radicalViewController.preferredContentSize = CGSize(width: 0.0, height: previewHeight)
+      viewController = radicalViewController
+    case .word(let word):
+      let wordDetailViewController: WordDetailViewController = WordDetailViewController.instantiateViewController()
+      wordDetailViewController.word = word
+      wordDetailViewController.navigationItem.title = word.character
+      let previewHeight = wordDetailViewController.view.bounds.width * 0.75 + 15
+      wordDetailViewController.preferredContentSize = CGSize(width: 0.0, height: previewHeight)
+      previewingContext.sourceRect = cell.frame
+      viewController = wordDetailViewController
     }
+    return viewController
+  }
 
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
-        presenter.pushViewController(viewControllerToCommit, animated: true)
-    }
+  func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+    presenter.pushViewController(viewControllerToCommit, animated: true)
+  }
 }
 
 // MARK: - DataBrowserViewControllerDelegate
 extension DataBrowserCoordinator: DataBrowserViewControllerDelegate {
-    func itemSelected(reviewItem: ReviewItem) {
-        switch reviewItem {
-            case .kanji(let kanji): showKanji(kanji: kanji)
-            case .radical(let radical): showRadical(radical: radical)
-            case .word(let word): showWord(word: word)
-        }
+  func itemSelected(reviewItem: ReviewItem) {
+    switch reviewItem {
+    case .kanji(let kanji): showKanji(kanji: kanji)
+    case .radical(let radical): showRadical(radical: radical)
+    case .word(let word): showWord(word: word)
     }
+  }
 
-    func searchTextDidChange(newText: String) {
-        searchDataProvider.searchText = newText
-    }
+  func searchTextDidChange(newText: String) {
+    searchDataProvider.searchText = newText
+  }
 
-    func searchCancelPressed() {
-        searchDataProvider.searchText = nil
-    }
+  func searchCancelPressed() {
+    searchDataProvider.searchText = nil
+  }
 }
