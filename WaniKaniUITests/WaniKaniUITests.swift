5647d3f00f64813dab2eb9ad95a3e30e3a820d0a
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-06-11 18:58:21 +0300

    snapshot and frame it

--- WaniKaniUITests/WaniKaniUITests.swift
+++ WaniKaniUITests/WaniKaniUITests.swift
@@ -51,13 +51,13 @@ class WaniKaniUITests: XCTestCase {
 //    acceptNotifications(app: app)
     login(app: app)
 
-    sleep(15)
+    sleep(25)
 
-    snapshot("Dashboard", waitForLoadingIndicator: false)
+    snapshot("1 Dashboard", waitForLoadingIndicator: false)
     XCUIApplication().scrollViews.otherElements.otherElements["progressView"].swipeLeft()
 
     sleep(1)
-    snapshot("SRS", waitForLoadingIndicator: false)
+    snapshot("2 SRS", waitForLoadingIndicator: false)
     sleep(1)
 
     var collectionViewsQuery = app.collectionViews
@@ -65,12 +65,17 @@ class WaniKaniUITests: XCTestCase {
 
     sleep(5)
 
-    XCUIApplication().toolbars.buttons["Done"].tap()
-    snapshot("Review", waitForLoadingIndicator: false)
+    if XCUIApplication().toolbars.buttons["Done"].exists {
+      XCUIApplication().toolbars.buttons["Done"].tap()
+    } else if XCUIApplication().buttons["Hide keyboard"].exists {
+      XCUIApplication().buttons["Hide keyboard"].tap()
+    }
+
+    snapshot("3 Review", waitForLoadingIndicator: false)
 
     app.toolbars.buttons["Strokes"].tap()
     sleep(2)
-    snapshot("Strokes", waitForLoadingIndicator: false)
+    snapshot("4 Strokes", waitForLoadingIndicator: false)
 
     sleep(2)
     app.toolbars.buttons["Submit"].tap()
@@ -87,17 +92,17 @@ class WaniKaniUITests: XCTestCase {
     sleep(10)
     collectionViewsQuery = app.collectionViews
     collectionViewsQuery.staticTexts["亅"].tap()
-    snapshot("Radical", waitForLoadingIndicator: false)
+    snapshot("5 Radical", waitForLoadingIndicator: false)
     app.navigationBars["亅"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
     collectionViewsQuery.children(matching: .cell).element(boundBy: 5).staticTexts["女"].tap()
-    snapshot("Kanji", waitForLoadingIndicator: false)
+    snapshot("6 Kanji", waitForLoadingIndicator: false)
     app.navigationBars["女"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
     collectionViewsQuery.staticTexts["下さい"].tap()
-    snapshot("Word", waitForLoadingIndicator: false)
+    snapshot("7 Word", waitForLoadingIndicator: false)
     app.navigationBars["下さい"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
 
     sleep(1)
-    snapshot("Search", waitForLoadingIndicator: false)
+    snapshot("8 Search", waitForLoadingIndicator: false)
     sleep(1)
     app.tabBars.buttons["Settings"].tap()
 
@@ -110,7 +115,7 @@ class WaniKaniUITests: XCTestCase {
 
     sleep(1)
 
-    snapshot("Settings", waitForLoadingIndicator: false)
+    snapshot("9 Settings", waitForLoadingIndicator: false)
   }
 
 }
